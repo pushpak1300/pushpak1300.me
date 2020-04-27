5fc7bdc0cbf31bdf963ea79dbbf381e34ef5b665
Pushpak Chhajes <pushpak1300@gmail.com>
2020-04-27 23:42:19 +0530

    Navbar toggler fixed

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -6,9 +6,9 @@
 //  */
 
 try {
-    // window.Popper = require('popper.js').default;
+    window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
-    // require('bootstrap');
+    require('bootstrap');
 } catch (e) {
     console.log(e);
 }
