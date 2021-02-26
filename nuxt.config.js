8c62c57dfc2a33ece79edc61486b1eec0f185c7b
pushpak1300 <pushpak1300@gmail.com>
2021-02-26 10:15:50 +0530

    Update linter fix ci

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,19 +9,37 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Pushpak\'s Portfolio' },
-      { name: 'msapplication-TileColor', content: "#da532c" },
-      { name: 'theme-color', content: "#ffffff" }
+      {
+        hid: 'description',
+        name: 'description',
+        content: "Pushpak's Portfolio",
+      },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffffff' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css', media: 'screen', defer: true },
+      {
+        rel: 'stylesheet',
+        href: 'https://rsms.me/inter/inter.css',
+        media: 'screen',
+        defer: true,
+      },
       { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16*16', href: '/favicon-16x16.png' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16*16',
+        href: '/favicon-16x16.png',
+      },
       { rel: 'manifest', type: 'image/x-icon', href: '/site.webmanifest' },
       { rel: 'mask-icon', color: '#5bbad5', href: '/safari-pinned-tab.svg' },
-
     ],
   },
   // Global CSS (https://go.nuxtjs.dev/config-css)
@@ -39,35 +57,33 @@ export default {
       {
         path: '~/components/Base/',
         prefix: 'Base',
-      }
-    ]
+      },
+    ],
   },
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     '@nuxtjs/eslint-module',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
-    ['@nuxtjs/google-analytics', {
-      id: 'UA-154733037-1'
-    }]
+    [
+      '@nuxtjs/google-analytics',
+      {
+        id: 'UA-154733037-1',
+      },
+    ],
   ],
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    '@nuxtjs/pwa',
-    '@nuxt/content',
-    '@nuxt/http',
-    '@nuxtjs/date-fns'
-  ],
+  modules: ['@nuxtjs/pwa', '@nuxt/content', '@nuxt/http', '@nuxtjs/date-fns'],
   // Content module configuration (https://go.nuxtjs.dev/config-content)
   content: {},
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
   router: {
     linkExactActiveClass: 'active-navlink',
-    middleware: 'redirect'
+    middleware: 'redirect',
   },
-  //dark mode configration
+  // dark mode configration
   colorMode: {
-    classSuffix: ''
-  }
+    classSuffix: '',
+  },
 }
