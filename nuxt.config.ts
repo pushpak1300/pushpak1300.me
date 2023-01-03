1156b58b36a2ce08bdb5b9bf76d1a2ae45547652
Pushpak Chhajed <pushpak1300@gmail.com>
2023-01-03 22:45:53 +0530

    feat: loading font asynchronous

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,8 @@ export default defineNuxtConfig({
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         {
-          rel: 'stylesheet',
+          rel: 'preload',
+          as: 'style',
           href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
           media: 'all'
         },
