212f944d68f2ece3bd6f9d8f14137975511ce77f
Pushpak Chhajed <pushpak1300@gmail.com>
2024-06-24 12:32:59 +0530

    feat: add symbolic file to script.sh

--- public/script.sh
+++ public/script.sh
@@ -1,1 +1,9 @@
-script.sh
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "gh-pages" ]] ; then
+    # Don't build
+    echo "🛑 - Build cancelled"
+    exit 0;
+fi
--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
-
-if [[ "$VERCEL_GIT_COMMIT_REF" == "gh-pages" ]] ; then
-    # Don't build
-    echo "🛑 - Build cancelled"
-    exit 0;
-fi
