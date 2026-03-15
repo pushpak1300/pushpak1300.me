978422f57bd22c97ba9843820f6ff1f8b0b58af8
Pushpak Chhajed <pushpak1300@gmail.com>
2026-03-15 16:00:38 +0530

    chore: ignore and remove bun.lockb (#748)

--- app.vue
+++ app.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script setup lang="ts">
-import { SpeedInsights } from '@vercel/speed-insights/nuxt';
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
 
 <style>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,8 @@
 // @ts-check
-import tailwind from 'eslint-plugin-tailwindcss';
 import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
 	rules: {
 		'no-unused-vars': 'error',
 	},
-	settings: {
-		tailwindcss: {
-			whitelist: ['.*primary.*'],
-		},
-	},
-}).append(...tailwind.configs['flat/recommended']);
+});
